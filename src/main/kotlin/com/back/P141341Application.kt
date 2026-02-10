9e266b6ad00ced0665040991b2dc31da6479cf77
Minji-032 <5h6vmm@gmail.com>
2026-02-10 11:15:23 +0900

    09

---
+++ src/main/kotlin/com/back/P141341Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class P141341Application
+
+fun main(args: Array<String>) {
+    runApplication<P141341Application>(*args)
+}
