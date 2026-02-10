91c08f27c8d993fc500fe2a5026cf94fe24e7a18
Minji-032 <5h6vmm@gmail.com>
2026-02-10 12:47:51 +0900

    21

--- src/main/kotlin/com/back/domain/chat/chatRoom/controller/ApiV1ChatRoomController.kt
+++ src/main/kotlin/com/back/domain/chat/chatRoom/controller/ApiV1ChatRoomController.kt
@@ -1,13 +1,22 @@
 package com.back.domain.chat.chatRoom.controller
 
 import com.back.domain.chat.chatRoom.entity.ChatRoom
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/v1/chat/rooms")
+@CrossOrigin(
+    origins = ["https://cdpn.io"]
+)
 class ApiV1ChatRoomController {
     val chatRooms: MutableList<ChatRoom> = mutableListOf(
         ChatRoom(
@@ -31,7 +40,35 @@ class ApiV1ChatRoomController {
     )
 
     @GetMapping
-    fun getItems() = chatRooms
+    fun getItems() = chatRooms.toList()
 
-    
+    @GetMapping("/{id}")
+    fun getItem(@PathVariable id: Int): ChatRoom? =
+        findById(id)
+
+    data class ChatCreateRoomReqBody(
+        val name: String
+    )
+
+    @PostMapping
+    fun createItem(
+        @RequestBody reqBody: ChatCreateRoomReqBody
+    ): ChatRoom {
+        val newId = (chatRooms.maxOfOrNull { it.id } ?: 0) + 1
+
+        val newChatRoom = ChatRoom(
+            newId,
+            LocalDateTime.now(),
+            LocalDateTime.now(),
+            reqBody.name
+        )
+
+        chatRooms.add(newChatRoom)
+
+        return newChatRoom
+    }
+
+    private fun findById(id: Int): ChatRoom? =
+        chatRooms.find { it.id == id }
+
 }
